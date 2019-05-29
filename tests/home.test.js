03862506ab97129be6a672fc8694c2165823d37e
Carl Pillot <carl.pillot@gmail.com>
2019-05-28 21:12:35 -0700

    Add home page tests

---
+++ tests/home.test.js
@@ -0,0 +1,22 @@
+describe('WHRB.org Home', () => {
+  beforeAll(async () => {
+    await page.goto('https://whrb.org');
+  });
+
+  it('should be titled "WHRB 95.3 FM - Harvard Radio Broadcasting"', async () => {
+    await expect(page.title()).resolves.toMatch('WHRB 95.3 FM - Harvard Radio Broadcasting');
+  });
+
+  it('should display the WHRB logo', async () => {
+    const logoImage = await page.$('a.brand img');
+    const title = await logoImage.getProperty('title');
+    const alt = await logoImage.getProperty('alt');
+
+    await expect(title.jsonValue()).resolves.toMatch('WHRB 95.3FM - Harvard Radio Broadcasting');
+    await expect(alt.jsonValue()).resolves.toMatch('WHRB Logo');
+  });
+
+  it('should display the main slider', async () => {
+    await expect(page.$('main.home section.main-content section.featured-display.whrb-panel')).resolves.toBeTruthy();
+  });
+});
